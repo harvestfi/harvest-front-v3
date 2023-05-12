b839d8de18133ad6f2a9664aa244862a3a852334
Alex <devstar0718@gmail.com>
2023-05-12 10:27:52 +0900

    Fix vertical align for chart on Analytic page.

--- src/components/AnalyticChart/style.js
+++ src/components/AnalyticChart/style.js
@@ -4,6 +4,7 @@ import { Modal } from 'react-bootstrap'
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
   width: 100%;
   height: 100%;
   overflow: hidden;
