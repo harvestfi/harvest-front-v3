e1690c69f22e3f7eba99083aced1641024207e80
Alex <devstar0718@gmail.com>
2023-06-07 23:50:18 +0900

    Code optimize for showing Profit Sharing APY.

--- src/components/ProfitSharing/index.js
+++ src/components/ProfitSharing/index.js
@@ -1,14 +1,8 @@
-import React, { useState, useEffect, useMemo } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
-import { displayAPY, getTotalApy, getDataQuery } from '../../utils'
-import {
-  FARM_TOKEN_SYMBOL,
-  SPECIAL_VAULTS,
-  DECIMAL_PRECISION,
-  directDetailUrl,
-} from '../../constants'
+import { getDataQuery } from '../../utils'
+import { directDetailUrl } from '../../constants'
 import { useStats } from '../../providers/Stats'
-import { usePools } from '../../providers/Pools'
 import { useWallet } from '../../providers/Wallet'
 import SmallApexChart from '../SmallApexChart'
 import { addresses } from '../../data/index'
@@ -17,26 +11,9 @@ import ProfitSharingIcon from '../../assets/images/logos/sidebar/profit-sharing.
 import ProfitSharingTitle from '../../assets/images/logos/sidebar/profit-sharing-title.svg'
 
 const ProfitSharingContainer = ({ height }) => {
-  const { pools } = usePools()
   const { chainId } = useWallet()
   const { profitShareAPY } = useStats()
   const { push } = useHistory()
-  const farmProfitSharingPool = pools.find(
-    pool => pool.id === SPECIAL_VAULTS.NEW_PROFIT_SHARING_POOL_ID,
-  )
-  const poolVaults = useMemo(
-    () => ({
-      [FARM_TOKEN_SYMBOL]: {
-        poolVault: true,
-        profitShareAPY,
-        data: farmProfitSharingPool,
-        logoUrl: ['./icons/ifarm.svg'],
-      },
-    }),
-    [profitShareAPY, farmProfitSharingPool],
-  )
-  const token = poolVaults.FARM
-  const totalApy = getTotalApy(null, token, true)
 
   const [apiData, setApiData] = useState({})
   useEffect(() => {
@@ -60,11 +37,11 @@ const ProfitSharingContainer = ({ height }) => {
       </TopDiv>
       <BottomDiv>
         <div className="apy">
-          {displayAPY(totalApy, DECIMAL_PRECISION, 10)}
-          &nbsp;APR
+          {Number(profitShareAPY).toFixed(2)}
+          %&nbsp;APR
         </div>
         <div className="chart">
-          <SmallApexChart data={apiData} lastAPY={Number(totalApy)} />
+          <SmallApexChart data={apiData} lastAPY={profitShareAPY} />
         </div>
       </BottomDiv>
     </ProfitSharing>
