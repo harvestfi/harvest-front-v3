e44c4ed8842190a551b6b0c0053d5bf96964b524
Alex <devstar0718@gmail.com>
2023-09-01 20:38:45 +0900

    Fix chart height of farm detail page.

--- src/components/WidoComponents/DetailChart/FarmDetailChart/style.js
+++ src/components/WidoComponents/DetailChart/FarmDetailChart/style.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  height: 400px;
+  min-height: 400px;
   overflow: hidden;
   padding: 0px;
   transition: 0.25s;
@@ -12,7 +12,6 @@ const Container = styled.div`
   @media screen and (max-width: 992px) {
     width: 100%;
     height: 400px;
-    margin-bottom: 15px;
   }
 `
 
