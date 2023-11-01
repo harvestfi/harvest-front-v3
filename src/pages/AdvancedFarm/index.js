3865c29123c507877ee42192dbea983748001393
Alex <devstar0718@gmail.com>
2023-11-01 19:37:45 +0900

    set farm token reward symbol to uppercase in Advanced farm page

--- src/pages/AdvancedFarm/index.js
+++ src/pages/AdvancedFarm/index.js
@@ -885,9 +885,9 @@ const AdvancedFarm = () => {
           (rewardToken.data &&
             rewardToken.data.lpTokenData &&
             rewardToken.data.lpTokenData.decimals)
-      } else if (rewardSymbol.substring(0, 1) === 'f') {
+      } else if (rewardSymbol.substring(0, 1) === 'F') {
         let underlyingRewardSymbol
-        if (rewardSymbol.substring(0, 2) === 'fx') {
+        if (rewardSymbol.substring(0, 2) === 'FX') {
           underlyingRewardSymbol = rewardSymbol.substring(2)
         } else {
           underlyingRewardSymbol = rewardSymbol.substring(1)
