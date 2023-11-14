ca968e64431816f6b56d6ab17427aa3b4a9c42bc
Alex <devstar0718@gmail.com>
2023-11-15 01:28:25 +0900

    show amount as the raw number in Stake/Unstake sections

--- src/components/AdvancedFarmComponents/Stake/StakeStart/index.js
+++ src/components/AdvancedFarmComponents/Stake/StakeStart/index.js
@@ -251,7 +251,13 @@ const StakeStart = ({
             >
               <NewLabel weight="500">{progressStep === 4 ? 'Staked' : 'Staking'}</NewLabel>
               <NewLabel display="flex" flexFlow="column" weight="600" align="right">
-                <>{inputAmount !== '' ? inputAmount : <AnimatedDots />}</>
+                <>
+                  {inputAmount !== '' ? (
+                    inputAmount.toFixed(18).replace(/\.?0+$/, '')
+                  ) : (
+                    <AnimatedDots />
+                  )}
+                </>
                 <span>{tokenSymbol !== '' ? `f${tokenSymbol}` : <AnimatedDots />}</span>
               </NewLabel>
             </NewLabel>
