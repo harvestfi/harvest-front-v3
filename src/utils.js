a83fc39787b619110d06b93cea395340d45fbc91
Alex <devstar0718@gmail.com>
2023-12-04 17:24:21 +0900

    change 'formatNumber' in utils

--- src/utils.js
+++ src/utils.js
@@ -150,10 +150,11 @@ export const formatNumber = (number, decimals = DECIMAL_PRECISION) => {
   let result = number
 
   if (countDecimals(result) > decimals) {
-    result = result.substring(0, result.indexOf('.') + decimals + 1)
+    // Round the number to the specified decimal places
+    result = Number(result.toFixed(decimals))
   }
 
-  return abbreaviteNumber(Number(result), decimals)
+  return abbreaviteNumber(result, decimals)
 }
 
 export const formatNumberWido = (number, decimals = DECIMAL_PRECISION) => {
