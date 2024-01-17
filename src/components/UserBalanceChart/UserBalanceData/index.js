732776afa44cc7e81a9c30d4cfa065d36e4e9db0
Alex <devstar718@gmail.com>
2024-01-17 17:11:39 +0100

    remove console log in Balance chart

--- src/components/UserBalanceChart/UserBalanceData/index.js
+++ src/components/UserBalanceChart/UserBalanceData/index.js
@@ -203,9 +203,9 @@ const UserBalanceData = ({
         mergedData.unshift(firstObject)
         // console.log('totalValue -------------', totalValue)
         // console.log('underlyingPrice -------------', underlyingPrice)
-        console.log('data1 -------------', data1)
-        console.log('data2 -------------', data2)
-        console.log('mergedData -------------', mergedData)
+        // console.log('data1 -------------', data1)
+        // console.log('data2 -------------', data2)
+        // console.log('mergedData -------------', mergedData)
       }
       setLoadComplete(flag1 && flag2)
       setApiData(mergedData)
