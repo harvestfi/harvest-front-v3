01b1b71133ef475e8c3e078a7f7fc48cafc4cd24
Alex <devstar718@gmail.com>
2024-02-01 22:53:39 +0900

    Fixed 'ALL' timeframe for farms with balance history less than 7 days

--- src/components/UserBalanceChart/ApexChart/index.js
+++ src/components/UserBalanceChart/ApexChart/index.js
@@ -353,8 +353,8 @@ const ApexChart = ({
       // filteredData.push(newObject)
 
       mainData = generateChartDataWithSlots(
-        range === 'ALL' ? filteredSlot : slots,
-        range === 'ALL' ? filteredData : data,
+        range === 'ALL' && ago > 6 ? filteredSlot : slots,
+        range === 'ALL' && ago > 6 ? filteredData : data,
         'value',
         'priceUnderlying',
         'sharePrice',
