b183db859852265182a94db87baa4390bedcb737
Alex <devstar718@gmail.com>
2024-03-01 17:46:47 +0900

    Fixed 'FLODE' issue in reward calculations

--- src/pages/AdvancedFarm/index.js
+++ src/pages/AdvancedFarm/index.js
@@ -800,9 +800,9 @@ const AdvancedFarm = () => {
 
         if (rewardTokenSymbols[l].substring(0, 1) === 'f') {
           if (rewardTokenSymbols[l] === 'fLODE') {
-            rewardToken = groupOfVaults.flodestar_LODE
+            rewardToken = groupOfVaults.lodestar_LODE
           } else if (rewardTokenSymbols[l] === 'fSUSHI') {
-            rewardToken = groupOfVaults.fSUSHI_HODL
+            rewardToken = groupOfVaults.SUSHI_HODL
           } else if (rewardTokenSymbols[l] === 'fDEN_4EUR') {
             rewardToken = groupOfVaults.jarvis_DEN_4EUR
           } else if (rewardTokenSymbols[l] === 'fDEN2_4EUR') {
