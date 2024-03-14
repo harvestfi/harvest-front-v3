3d36bd905b4f88c5488d1cf1ea76af96746ac942
Alex <devstar718@gmail.com>
2024-03-14 22:49:02 +0900

    New content for the Tutorial page(3)

--- src/pages/Tutorial/index.js
+++ src/pages/Tutorial/index.js
@@ -350,7 +350,8 @@ const Home = () => {
             <DescText>
               It is important to note that this box provides a live valuation on our Underlying
               Deposit, which is constantly exposed to market volatility (even in stablecoin farms,
-              My Balance and the green line on the chart can fluctuate by a small margin).
+              My Balance and the green line on the chart can fluctuate by a small margin). The
+              beginner farm, which auto-compounds ETH, can see its USD valuation go up and down.
               <br />
               <br />
               At the end of the day, Harvest auto-compounds ETH into more ETH units for farmers
