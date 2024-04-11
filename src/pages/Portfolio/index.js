bcca114ee8bdd184307940d2086be366eee8fa6f
CryptJS13 <jasper@slimmens.nl>
2024-04-11 18:19:05 +0200

    Fix farm token balance dashboard

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -406,7 +406,7 @@ const Portfolio = () => {
             }
             const totalStk = parseFloat((isNaN(Number(stake)) ? 0 : parseValue(stake)) * usdPrice)
             const totalUnsk = parseFloat(
-              (isNaN(Number(unstake)) ? 0 : parseValue(unstake)) * usdPrice,
+              (isNaN(Number(unstake)) || useIFARM ? 0 : parseValue(unstake)) * usdPrice,
             )
             totalBalanceUSD += totalStk + totalUnsk
             const rewardTokenSymbols = get(fAssetPool, 'rewardTokenSymbols', [])
