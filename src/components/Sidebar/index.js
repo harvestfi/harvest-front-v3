852ee2cca3f7fe4c05a24a1a69be3a6e64b4766c
Alex <devstar718@gmail.com>
2024-05-20 02:05:01 +0900

    Fix UI issues of active items in sidebar & mobile menu

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -235,11 +235,11 @@ const SideLink = ({ item, subItem, isDropdownLink, fontColor2, activeIconColor, 
   const { pathname } = useLocation()
   const pageName =
     pathname === '/'
-      ? 'home'
-      : pathname === ROUTES.ADVANCED
-      ? 'advanced'
-      : pathname === ROUTES.PORTFOLIO
       ? 'dashboard'
+      : pathname === ROUTES.ADVANCED
+      ? 'all farms'
+      : pathname === ROUTES.TUTORIAL
+      ? 'tutorial'
       : pathname
   return (
     /* eslint-disable-next-line jsx-a11y/anchor-is-valid */
@@ -278,7 +278,8 @@ const MobileMenu = ({
   isMobile,
 }) => {
   const { pathname } = useLocation()
-  const pageName = pathname === '/' ? 'home' : pathname
+  const pageName =
+    pathname === '/' ? 'dashboard' : pathname === ROUTES.ADVANCED ? 'all farms' : pathname
   return (
     <LinkMobile
       fontColor={fontColor}
@@ -1077,7 +1078,7 @@ const Sidebar = ({ width }) => {
               }
             >
               <MobileToggle
-                className={`wallet-btn ${connected && 'connected-wallet-btn'}`}
+                className="wallet-btn"
                 toggleColor={toggleColor}
                 width={27}
                 height={21}
