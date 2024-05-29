77c17517faf3fa270b051bb4b62d320c6901261b
CryptJS13 <cryptjs13@protonmail.com>
2024-05-29 20:38:58 +0200

    Fix FARM->iFARM deposit

--- src/pages/AdvancedFarm/index.js
+++ src/pages/AdvancedFarm/index.js
@@ -350,7 +350,7 @@ const AdvancedFarm = () => {
         data: farmProfitSharingPool,
         logoUrl: ['./icons/ifarm.svg'],
         tokenAddress: addresses.FARM,
-        vaultAddress: addresses.FARM,
+        vaultAddress: addresses.iFARM,
         rewardSymbol: 'iFarm',
         decimals: 18,
         tokenNames: ['FARM'],
