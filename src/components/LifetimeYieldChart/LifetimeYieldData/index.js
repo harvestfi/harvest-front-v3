916306033c9606ac6b29cc56ccc7596fdb122336
Alex <devstar718@gmail.com>
2024-12-04 17:32:59 +0900

    Add a tooltip to lifetime yield on dashboard

--- src/components/LifetimeYieldChart/LifetimeYieldData/index.js
+++ src/components/LifetimeYieldChart/LifetimeYieldData/index.js
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import { PiQuestion } from 'react-icons/pi'
+import ReactTooltip from 'react-tooltip'
+import { useMediaQuery } from 'react-responsive'
 import { useThemeContext } from '../../../providers/useThemeContext'
 import { useWallet } from '../../../providers/Wallet'
 import { useRate } from '../../../providers/Rate'
-import { Container, Header, ChartDiv, TooltipContent, CurDate } from './style'
+import { Container, Header, ChartDiv, TooltipContent, CurDate, NewLabel } from './style'
 import ApexChart from '../ApexChart'
 import { formatDate, numberWithCommas } from '../../../utilities/formats'
 
 const LifetimeYieldData = ({ noData, totalHistoryData }) => {
   const { connected } = useWallet()
-  const { fontColor } = useThemeContext()
+  const { darkMode, fontColor } = useThemeContext()
   const [curDate, setCurDate] = useState('')
   const [curContent, setCurContent] = useState('')
+  const isMobile = useMediaQuery({ query: '(max-width: 992px)' })
 
   const nowDate = new Date()
   const currentTimeStamp = Math.floor(nowDate.getTime() / 1000)
@@ -51,7 +54,29 @@ const LifetimeYieldData = ({ noData, totalHistoryData }) => {
         <TooltipContent>
           <div className="title">
             Lifetime Yield
-            <PiQuestion />
+            <PiQuestion className="question" data-tip data-for="lifetime-yield-desktop" />
+            <ReactTooltip
+              id="lifetime-yield-desktop"
+              backgroundColor={darkMode ? 'white' : '#101828'}
+              borderColor={darkMode ? 'white' : 'black'}
+              textColor={darkMode ? 'black' : 'white'}
+              className="mobile-top-tooltip"
+              place="top"
+            >
+              <NewLabel
+                size={isMobile ? '10px' : '12px'}
+                height={isMobile ? '15px' : '18px'}
+                weight="600"
+              >
+                Your wallet&apos;s lifetime yield with Harvest originating from &apos;harvest&apos;
+                events. It shows the value of your accumulated yield in the currency you chose (USD,
+                EUR, etc.) at the time of each harvest event.
+                <br />
+                <br />
+                Note: This does not include Claimable Rewards or yield originating from Liquidity
+                Provision.
+              </NewLabel>
+            </ReactTooltip>
           </div>
           <div className="content">
             <div dangerouslySetInnerHTML={{ __html: connected ? curContent : '' }} />
