4aedcd9519c9e7ae9ec4e97ee808aaa205252f87
CryptJS13 <cryptjs13@protonmail.com>
2025-07-17 17:45:19 +0200

    Update loadEnv

--- config/loadEnv.js
+++ config/loadEnv.js
@@ -3,17 +3,19 @@ const path = require('path')
 const dotenv = require('dotenv')
 
 module.exports = () => {
+  // 1) resolve the path to your .env file
   const envPath = path.resolve(__dirname, '../.env')
 
-  const envFile = fs.existsSync(envPath) ? dotenv.config({ path: envPath }) : { parsed: {} }
-  const envVars = envFile.parsed || {}
+  // 2) if it exists, load its vars into process.env
+  if (fs.existsSync(envPath)) {
+    dotenv.config({ path: envPath })
+  }
 
-  const formatted = Object.entries(envVars)
+  // 3) export a function that picks up all REACT_APP_* from process.env
+  return Object.entries(process.env)
     .filter(([key]) => key.startsWith('REACT_APP_'))
     .reduce((acc, [key, val]) => {
       acc[`process.env.${key}`] = JSON.stringify(val)
       return acc
     }, {})
-
-  return formatted
 }
