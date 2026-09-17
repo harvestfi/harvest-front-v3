0256650da43addd6f8a881fdfe42940bbc455adc
William Liu <williamliu9426@gmail.com>
2026-09-17 16:56:29 -0400

    fix: revert fee show

--- src/pages/AdvancedFarm/index.js
+++ src/pages/AdvancedFarm/index.js
@@ -842,6 +842,14 @@ const AdvancedFarm = () => {
     }
   }, [exitMechanics, tokenChain])
 
+  const vaultFeeList = useMemo(() => {
+    if (!(Number(exitFeeBps) > 0)) {
+      return feeList
+    }
+    const exitFeeLabel = `${new BigNumber(exitFeeBps).div(100).decimalPlaces(3).toString()}%`
+    return feeList.map(fee => (fee.key === 'revert' ? { ...fee, value: exitFeeLabel } : fee))
+  }, [exitFeeBps])
+
   const mainTags = [
     { name: 'Manage', img: Safe },
     { name: 'Rewards', img: Diamond },
@@ -3537,7 +3545,7 @@ const AdvancedFarm = () => {
                       >
                         Fees
                       </NewLabel>
-                      {feeList.map((feeItem, index) => (
+                      {vaultFeeList.map((feeItem, index) => (
                         <FlexDiv
                           key={index}
                           $justifycontent="space-between"
